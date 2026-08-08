699a009be21165a3930f38a543ff9ae819f6c982
pieman492 <rauljrrauljr@gmail.com>
2026-08-08 11:34:43 -0500

    Move particles to datagen

--- src/main/java/dev/dhyces/lunarnether/datagen/client/ModParticleDescriptionProvider.java
+++ src/main/java/dev/dhyces/lunarnether/datagen/client/ModParticleDescriptionProvider.java
@@ -1,8 +1,11 @@
 package dev.dhyces.lunarnether.datagen.client;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.common.data.ParticleDescriptionProvider;
+
+import dev.dhyces.lunarnether.registry.ModParticleTypes;
 
 public class ModParticleDescriptionProvider extends ParticleDescriptionProvider {
     public ModParticleDescriptionProvider(PackOutput packOutput, ExistingFileHelper existingFileHelper) {
@@ -11,5 +14,9 @@ public class ModParticleDescriptionProvider extends ParticleDescriptionProvider 
 
     @Override
     protected void addDescriptions() {
+        spriteSet(
+            ModParticleTypes.COLORED_ASH.get(),
+            ResourceLocation.withDefaultNamespace("generic_0")
+        );
     }
 }
